416a80f47472cc34627edc785f97444aa86c08e8
g14n4p3 <g14n4p3@g14n4p3s-MBP.fritz.box>
2022-11-24 20:47:55 +0100

    adding swal

--- apps/wallet/src/app/app.module.ts
+++ apps/wallet/src/app/app.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ModalComponent } from './components/modal/modal.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent, LoginComponent, ModalComponent],
+  declarations: [AppComponent, NxWelcomeComponent, LoginComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
--- apps/wallet/src/app/pages/login/login.component.ts
+++ apps/wallet/src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import Swal from 'sweetalert2';
 
 @Component({
     selector: 'wallet-login',
@@ -6,5 +7,36 @@ import { Component } from "@angular/core";
     styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+    public constructor() {}
+
+    public alertConfirmation() {
+        const text: string = `
+        <table class="min-w-full text-sm">
+            <tr>
+                <td class="w-1/5 text-left bg-slate-200 p-2">logginKey: </td>
+                <td class="w-1/1 text-left bg-slate-200 p-2">adf13nD&%34523413409dfgsf</td>
+            </tr>
+            <tr>
+                <td class="w-1/5 text-left p-2">privateKey:</td>
+                <td class="w-1/1 text-left p-2">DSjgasdhG9134bd84blalsd61445hjvadf</td>
+            </tr>
+            <tr>
+                <td class="w-1/5 text-left bg-slate-200 p-2">publicKey </td>
+                <td class="w-1/1 text-left bg-slate-200 p-2">0x8761349875346078410384760871324</td>
+            </tr>
+            <tr>
+                <td class="w-1/5 text-left p-2">2FaKey: </td>
+                <td class="w-1/1 text-left p-2">ASDFLBJ086408</td>
+            </tr>
+          </table>`;
+
+        Swal.fire({
+            title: 'Wallet has been created!',
+            html: text,
+            icon: 'success',
+            showCancelButton: false,
+            confirmButtonText: 'Download file',
+        });
+    }
 
 }
